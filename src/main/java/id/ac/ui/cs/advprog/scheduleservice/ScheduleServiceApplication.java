37367184da13f62a63bd36b3158e9878e659d524
Bimasena Putra <bimasena.putra@ui.ac.id>
2022-03-27 23:29:18 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/scheduleservice/ScheduleServiceApplication.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.scheduleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ScheduleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/scheduleservice/ScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.scheduleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
