1de130bb8f5c860cd29d634144ee3b77a53f93a7
Bimasena Putra <bimasena.putra@ui.ac.id>
2022-04-02 17:05:12 +0700

    Change port

--- src/main/java/id/ac/ui/cs/advprog/scheduleservice/ScheduleServiceApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/scheduleservice/ScheduleServiceApplication.java
@@ -2,10 +2,8 @@ package id.ac.ui.cs.advprog.scheduleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ScheduleServiceApplication {
 
     public static void main(String[] args) {
